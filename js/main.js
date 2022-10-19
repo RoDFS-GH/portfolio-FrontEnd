b08d857b5f809369f56943a7b1786ab97f71fb19
RodDFS-GH <rosana.19abril@gmail.com>
2022-10-19 10:09:21 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* JS Document */
+
+
+// funcionalidad flecha de desplazar hacia arriba
+
+const desplazarArriba = document.getElementById("desplazarse-hacia-arriba");
+
+desplazarArriba.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
