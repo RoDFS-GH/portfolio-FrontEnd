954a983a297b04cfd90913605efba53428767fb7
RodDFS-GH <rosana.19abril@gmail.com>
2023-06-02 13:44:00 -0300

    Primer Commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 /* JS Document */
 
+//validaciones
+
+import { valida } from "./validaciones.js";
+
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", (input) => {
+    valida(input.target);
+  });
+});
 
 // funcionalidad flecha de desplazar hacia arriba
 
